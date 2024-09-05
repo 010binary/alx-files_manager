89026ed90575c94a5d76b718ea88856e84ba865f
010binary <augustine32fg@gmail.com>
2024-09-05 04:23:30 -0700

    linter

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import routes from './routes/index';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json({ limit : '5mb' }));
+app.use(express.json({ limit: '5mb' }));
 app.use(routes);
-app.listen(PORT)
+app.listen(PORT);
